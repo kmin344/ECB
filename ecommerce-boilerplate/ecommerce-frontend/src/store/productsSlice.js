63e4777a8a226e0908469da101bbcb055bd6293b
kmin344 <kmin.kim00@gmail.com>
2024-09-22 20:29:58 +0900

    Refactor productsSlice.js to use custom API service for fetching products

--- ecommerce-boilerplate/ecommerce-frontend/src/store/productsSlice.js
+++ ecommerce-boilerplate/ecommerce-frontend/src/store/productsSlice.js
@@ -1,10 +1,10 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
-import axios from 'axios';
+import { getProducts } from '../services/api';
 
 export const fetchProducts = createAsyncThunk(
   'products/fetchProducts',
   async () => {
-    const response = await axios.get('/api/products');
+    const response = await getProducts('/api/products');
     return response.data;
   }
 );
