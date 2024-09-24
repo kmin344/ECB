82991d11c139fd1623cc0bca3149c4ab70004d5f
kmin344 <kmin.kim00@gmail.com>
2024-09-24 20:04:27 +0900

    Refactor Header component to move logo link to the right side

--- ecommerce-boilerplate/ecommerce-frontend/src/components/Header.js
+++ ecommerce-boilerplate/ecommerce-frontend/src/components/Header.js
@@ -11,7 +11,9 @@ const Header = () => {
           <li><Link to="/contact">Contact</Link></li>
         </ul>
       </nav>
-      <Link to="/" className="text-2xl font-bold">Logo</Link>
+      <Link to="/" className="text-2xl font-bold">
+        <img className="scroll_logo fixed_transform" src="https://cdn.imweb.me/thumbnail/20240604/4f4d4fc80e8be.png" alt="아뜰리에페이(Atelier Faye)" width="151.304347826" />
+      </Link>
       <div className="flex space-x-4">
         <button>Search</button>
         <button>Account</button>
