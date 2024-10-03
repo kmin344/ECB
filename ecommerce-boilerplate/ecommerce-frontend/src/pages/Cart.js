b1cbb102a0d3f21268d8e9d59c582c6d9b1bc611
kmin344 <kmin.kim00@gmail.com>
2024-10-03 23:44:08 +0900

    Refactor import statements in Cart component to use updated file path for cartSlice

--- ecommerce-boilerplate/ecommerce-frontend/src/pages/Cart.js
+++ ecommerce-boilerplate/ecommerce-frontend/src/pages/Cart.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { removeFromCart, updateQuantity, clearCart } from '../redux/cartSlice';
+import { removeFromCart, updateQuantity, clearCart } from '../store/cartSlice';
 import { Link } from 'react-router-dom';
 
 const Cart = () => {
