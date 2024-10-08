d9b0c218f1ae8623df685bbc0ce7bac60ee840c8
kmin344 <kmin.kim00@gmail.com>
2024-10-08 20:52:19 +0900

    Refactor user.model.js to hash passwords before saving and add method to compare passwords for login

--- ecommerce-boilerplate/services/user-service/src/models/user.model.js
+++ ecommerce-boilerplate/services/user-service/src/models/user.model.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 
-const UserSchema = new mongoose.Schema({
+const userSchema = new mongoose.Schema({
   username: {
     type: String,
     required: true,
@@ -31,4 +32,22 @@ const UserSchema = new mongoose.Schema({
   }
 });
 
-module.exports = mongoose.model('User', UserSchema);
+// Hash the password before saving
+userSchema.pre('save', async function(next) {
+  if (!this.isModified('password')) return next();
+  
+  try {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+  } catch (error) {
+    next(error);
+  }
+});
+
+// Method to compare password for login
+userSchema.methods.comparePassword = async function(candidatePassword) {
+  return bcrypt.compare(candidatePassword, this.password);
+};
+
+module.exports = mongoose.model('User', userSchema);
