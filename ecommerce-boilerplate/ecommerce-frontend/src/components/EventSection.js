f501fa8f89b7915e9200cb7106c4ec7b1d84033c
kmin344 <kmin.kim00@gmail.com>
2024-10-26 23:46:50 +0900

    Add missing product to EventSection.js

--- ecommerce-boilerplate/ecommerce-frontend/src/components/EventSection.js
+++ ecommerce-boilerplate/ecommerce-frontend/src/components/EventSection.js
@@ -9,7 +9,6 @@ const EventSection = () => {
     { id: 3, name: "Product 3", price: 39000, image: "https://cdn.imweb.me/thumbnail/20240607/ebc866450cfd0.jpg" },
     { id: 3, name: "Product 3", price: 39000, image: "https://cdn.imweb.me/thumbnail/20240607/ebc866450cfd0.jpg" },
     { id: 3, name: "Product 3", price: 39000, image: "https://cdn.imweb.me/thumbnail/20240607/ebc866450cfd0.jpg" },
-    { id: 3, name: "Product 3", price: 39000, image: "https://cdn.imweb.me/thumbnail/20240607/ebc866450cfd0.jpg" },
     // Add more products as needed
   ];
 
